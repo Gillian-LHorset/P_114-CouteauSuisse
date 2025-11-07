b68eb2ab68c6a32b7cb398401984148953cb1552
Gillian-LHorset <pq60soi@eduvaud.ch>
2025-11-07 11:20:30 +0100

    feat(Code): ajout d'un programme pour convertir une phrase en morse

---
+++ Code-P_114-CouteauSuisse/Code-P_114-CouteauSuisse/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Code_P_114_CouteauSuisse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] morseArray = new string[]
+            {
+                ".-",   "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+                "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+                "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", "·----", "··---", "···--", 
+                "····-", "·····", "-····", "--···", "---··", "----·", "/"
+            };
+
+            char[] lettreArray = new char[]
+            {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+                'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', 
+                '4', '5', '6', '7', '8', '9', ' '
+            };
+
+            int y = 0;
+            string userPhrase;
+
+            bool isKnowChar = false;
+            bool aCharIsUnknow = false;
+
+            Console.WriteLine("Merci d'enter une phrase sans accents et caratères spéciaux.");
+
+            userPhrase = Console.ReadLine();
+            while (userPhrase.Length < 1)
+            {
+                Console.WriteLine("Merci d'entrer une phrase.");
+                userPhrase = Console.ReadLine();
+            }
+
+            Console.Clear();
+
+            Console.CursorVisible = false;
+
+            Console.WriteLine($"Votre phrase était " + '"' + userPhrase + '"' + "\n");
+            Console.WriteLine("Votre phrase en morse donne : ");
+            foreach (char lettreInPhrase in userPhrase.ToUpper())
+            {
+                isKnowChar = false;
+
+                for (int i = 0; i < lettreArray.Length; i++)
+                {
+                    if (CompareChar(lettreInPhrase, lettreArray[i]))
+                    {
+                        Console.Write(morseArray[i] + " ");
+                        isKnowChar = true;
+                        break;
+                    }
+                }
+                if (!isKnowChar)
+                // si le char n'est pas présent dans la liste de lettre
+                {
+                    // écrit un ?
+                    Console.Write("? ");
+                    // signale qu'un caractère inconnue à été détecté
+                    aCharIsUnknow = true;
+                }
+            }
+
+            Console.WriteLine("\nUn ou plusieurs caratères de sont pas pris en charge.\nLes caractères inconnues ont été remplacer par le symbole " + '"' + '?' + '"' + '.');
+
+            Console.ReadKey();
+        }
+
+        public static bool CompareChar(char firstLettre, char secondLettre)
+        {
+            if (firstLettre == secondLettre)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        
+    }
+}
+
